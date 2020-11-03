66cdcd9186b35eefd21dd133fe233f7cc4853970
wangxingang4 <wangxingang@jd.com>
2020-11-03 18:03:35 +0800

    123

---
+++ day01/src/com/jdk/Demo.java
@@ -0,0 +1,33 @@
+package com.jdk;
+
+import java.util.function.DoubleToIntFunction;
+import java.util.function.DoubleToLongFunction;
+import java.util.function.Function;
+
+public class Demo {
+    public static void main(String[] args) {
+//        int num = 0;
+//        Runnable r = () ->{
+//            System.out.println(num);
+//        };
+//
+//        new Thread(r).start();
+
+
+
+
+
+
+
+
+
+        DoubleToIntFunction doubleToIntFunction = new DoubleToIntFunction() {
+            @Override
+            public int applyAsInt(double value) {
+                return 0;
+            }
+        };
+
+
+    }
+}
